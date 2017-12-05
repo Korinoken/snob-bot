53bc49040caae52995234aa1142c99c0ff51a08a
Korinoken <nikolajs.pelniks@gmail.com>
2017-12-06 01:27:51 +0200

    Just experimenting

--- bot.go
+++ bot.go
@@ -4,12 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"log"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+	//	"time"
+	"time"
 )
-
 
 func init() {
 	flag.StringVar(&token, "t", "", "Bot Token")
@@ -47,13 +49,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 	if strings.HasPrefix(m.Content, "!help") {
-		s.ChannelMessageSend(m.ChannelID,"Receiving help will only weaken you")
+		s.ChannelMessageSend(m.ChannelID, "Receiving help will only weaken you")
 	}
 	if strings.HasPrefix(m.Content, "!join") {
-		err := listenChannel(s,m)
+		err := listenChannel(s, m)
 
 		if err != nil {
-			s.ChannelMessageSend(m.ChannelID,fmt.Sprintf("Cannot join channel: %v",err))
+			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Cannot join channel: %v", err))
 		}
 	}
 
@@ -71,9 +73,62 @@ func listenChannel(s *discordgo.Session, m *discordgo.MessageCreate) error {
 	}
 	for _, vs := range g.VoiceStates {
 		if vs.UserID == m.Author.ID {
-			_, err := s.ChannelVoiceJoin(g.ID, vs.ChannelID, true, true)
+			log.Printf("joined channel: %v", vs.ChannelID)
+			vc, err := s.ChannelVoiceJoin(g.ID, vs.ChannelID, false, false)
+			vc.AddHandler(listenVoice)
 			return err
 		}
 	}
+
 	return nil
+}
+func listenVoice(vc *discordgo.VoiceConnection, vs *discordgo.VoiceSpeakingUpdate) {
+	if vc.UserID == vs.UserID {
+		return
+	}
+	log.Print("Listen voice started")
+
+	allVoice := make([]discordgo.Packet, 0)
+LISTENING:
+	for vs.Speaking == true {
+		select {
+		case voicePacket := <-vc.OpusRecv:
+			allVoice = append(allVoice, *voicePacket)
+			log.Printf("timestamp: %v, user: %v,event: %v , stored packets: %v",
+				voicePacket.Timestamp,
+				vs.UserID,
+				vs.SSRC,
+				len(allVoice))
+		case <-time.After(500 * time.Millisecond):
+			log.Println("timed out")
+			break LISTENING
+		}
+		//packetCopy := make([]byte,len(voicePacket.Opus))
+		//copy(packetCopy,voicePacket.Opus)
+
+	}
+	log.Printf("Speaking event ended, stored voice :%v", len(allVoice))
+	time.Sleep(2 * time.Second)
+	log.Printf("Done sleeping")
+	if vc.Ready {
+		if len(allVoice) > 5 {
+			err := vc.Speaking(true)
+			if err == nil {
+
+				for _, voicePacket := range allVoice {
+					log.Printf("Sending packet %v", voicePacket.Sequence)
+					vc.OpusSend <- voicePacket.Opus
+				}
+				time.Sleep(500 * time.Millisecond)
+				vc.Speaking(false)
+
+			} else {
+				log.Printf("Cannot start speaking %v", err)
+			}
+			log.Print("Done sending")
+		}
+	} else {
+		log.Print("Sent nothing")
+	}
+
 }
